92c9d23300377905736f0432ed2ea466f36c82c0
Felipe <felipe@toke.com>
2022-09-04 21:25:10 -0300

    WORKING

--- src/main/kotlin/br/com/peixinho_karaoke/models/request/delete_request/DeleteRequestDTO.kt
+++
@@ -1,8 +0,0 @@
-package br.com.peixinho_karaoke.models.request.delete_request
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class DeleteRequestDTO(
-    val request_id: Int?,
-)
--- src/main/kotlin/br/com/peixinho_karaoke/models/request/search/SearchDTO.kt
+++
@@ -1,8 +0,0 @@
-package br.com.peixinho_karaoke.models.request.search
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-class SearchDTO(
-    val searchString: String?,
-)
